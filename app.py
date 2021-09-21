5b26f118807398b5cbae3777613ece297b424015
David Christian <chradavi@amazon.com>
2021-09-20 21:51:06 -0700

    hopefully last db change

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import json
+from logging import currentframe
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 import redis
 from db import db
 import mysql.connector
+from mysql.connector import errorcode
 
 app = Flask(__name__)
 cors = CORS(app, resources={f"*": {"origins": "*"}})
@@ -19,56 +21,69 @@ db_user = os.environ.get('DB_USER', 'root')
 db_passwd = os.environ.get('DB_PASSWORD', 'myAwesomePassword')
 db_name = os.environ.get('DATABASE', 'mydb')
 
-
-def create_db():
-    mydb = mysql.connector.connect(
+mydb = mysql.connector.connect(
         host=db_host,
         user=db_user,
         password=db_passwd
     )
+cursor = mydb.cursor()
 
-    mycursor = mydb.cursor()
-    mycursor.execute(f"CREATE DATABASE IF NOT EXISTS `mydb`;")
-    mycursor.execute("""
-        USE `mydb`;
+
+def create_db(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(db_name))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(db_name))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_db(cursor)
+        print("Database {} created successfully.".format(db_name))
+        mydb.database = db_name
+    else:
+        print(err)
+        exit(1)
+
+def create_table():
+    try:
+        print("Creating table posts")
+        cursor.execute("""
         CREATE TABLE IF NOT EXISTS `posts`(
             `thread` int NOT NULL,
             `text` varchar(50) NOT NULL,
             `user` int NOT NULL,
             PRIMARY KEY (`thread`)
             );
-        """,multi=True)
-
-    try:
-        mycursor.execute("""    
-            USE `mydb`;
-
+        """)
+        
+        cursor.execute("""
             insert into `posts`(`thread`,`text`,`user`) 
-                values (1,'Has anyone checked on the lich recently?',1);
-                """,multi=True)
-    except:
-        print("error inserting row")
-create_db()
+                values (1,'Hello Im Alive!',1);
+        """)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
 
-conn = None
-if not local_db:
-    try:
-        create_db()
-        conn = mysql.connector.connect(host=db_host, user=db_user, passwd=db_passwd, database=db_name)
-    except:
-        print("unable to connect to MySQL")
-
+create_table()
+    
 def get_from_db(table):
     if local_db:
         return db.get(table)
-
-    
-    print(f"starting: conn cursor")
-    cursor = conn.cursor()
-    print(f"starting: SELECT * FROM {table}")
-    cursor.execute(f"SELECT * FROM {table}")
-    return cursor.fetchall()
-
+    try:
+        cursor.execute(f"SELECT * FROM {table}")
+        return cursor.fetchall()
+    except Exception as error:
+        return "SQL error running: " + str(error)
+ 
 
 @app.route('/api/posts', methods=['GET'], strict_slashes=False)
 def posts():
